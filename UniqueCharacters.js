5b96310c90c0b3fed6adaf0e7bf066eee452c588
polon59 <przemyslaw.polczak@gmail.com>
2019-05-10 12:02:50 +0200

    Add unique characters finder

---
+++ UniqueCharacters.js
@@ -0,0 +1,26 @@
+// Given string (random phrase or whatever). Return another string with only unique characters.
+
+const checkIfCharactersEqual = (a,b)=>{
+    return a === b;
+}
+
+const findUniqueChars = (word) =>{
+    const arrFromWord = word.split('');
+    let unique = [];
+
+    arrFromWord.forEach((letter)=>{
+        let isLetterInList = false;
+        unique.forEach((uniqueChar)=>{
+            if (letter === uniqueChar) {
+                isLetterInList = true;
+            }
+        })
+        if (!isLetterInList) {
+            unique.push(letter);
+        }
+    })
+
+    return unique;
+}
+
+console.log(findUniqueChars("abcdedeaaf"));
