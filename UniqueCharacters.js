5a6a489f56ca46dd62a9cf18eeb625c4f497af98
polon59 <przemyslaw.polczak@gmail.com>
2019-05-14 16:04:26 +0200

    Add performance counter in 'UniqueCharacters.js'

--- UniqueCharacters.js
+++ UniqueCharacters.js
@@ -1,22 +1,27 @@
 // Given string (random phrase or whatever). Return another string with only unique characters.
 
+const {performance} = require('perf_hooks');
+
 const findUniqueChars = (word) =>{
     const arrFromWord = word.split('');
-    let unique = [];
-
+    let uniqueCharacters = [];
     arrFromWord.forEach((letter)=>{
         let isLetterInList = false;
-        unique.forEach((uniqueChar)=>{
+        uniqueCharacters.forEach((uniqueChar)=>{
             if (letter === uniqueChar) {
                 isLetterInList = true;
             }
         })
         if (!isLetterInList) {
-            unique.push(letter);
+            uniqueCharacters.push(letter);
         }
     })
-
-    return unique;
+    return uniqueCharacters;
 }
 
-console.log(findUniqueChars("abcdabcde"));
+
+const startTime = performance.now();
+const result = findUniqueChars("piniendzy ni ma bo jest zima");
+const stopTime = performance.now();
+console.log(result);
+console.log(`Call to function 'findUniqueChars' took ${stopTime-startTime} milliseconds.`);
