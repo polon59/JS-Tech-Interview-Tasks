a88708abdf196d50485748cf0bf6740975fb04b2
polon59 <przemyslaw.polczak@gmail.com>
2019-05-27 18:59:28 +0200

    Add button to get new user data

--- React fetch (task 5)/react_fetch/src/App.js
+++ React fetch (task 5)/react_fetch/src/App.js
@@ -10,21 +10,21 @@ class App extends Component{
   }
 
   fetchNewUserData = () =>{
-    return fetch('https://randomuser.me/api/', 
+    fetch('https://randomuser.me/api/', 
     {method : "GET",
       headers: {
         'Accept' : 'application/json',
         'Content-Type': 'application/json'
       }
     }).then(response => response.json())
-    .then(data => {return data})
+    .then(data => {this.setState({currentUser:data})})
     .catch(err => console.log(err.message))
   }
 
+
+
   componentDidMount(){
-    this.fetchNewUserData().then( (data) =>{
-      this.setState({currentUser:data})
-    })
+    this.fetchNewUserData();
   }
 
 
@@ -42,15 +42,18 @@ class App extends Component{
       )
     }
 
-    const {name,picture} = currentUser.results[0];
+    const {name,picture,location} = currentUser.results[0];
 
     return(
       <div className="App">
         <p>
           {JSON.stringify(currentUser)}
         </p>
-        <h4>{`Name : ${name.title} ${name.first} ${name.last}`}</h4>
+        <h4>{`NAME : ${name.title} ${name.first} ${name.last}`}</h4>
+        <h4>{`LOCATION : State ${location.state}, ${location.city} City, ${location.street}`}</h4>
+        
         <img src={picture.large} alt="image"/>
+        <button onClick={this.fetchNewUserData}>Find new user</button>
       </div>
     )
   }
