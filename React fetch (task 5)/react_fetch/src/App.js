b85bdf4617b14b3889a7285dc85bb825e7afb80b
polon59 <przemyslaw.polczak@gmail.com>
2019-05-27 18:42:50 +0200

    Implement 'fetchNewUserData' method in 'App.js'

--- React fetch (task 5)/react_fetch/src/App.js
+++ React fetch (task 5)/react_fetch/src/App.js
@@ -9,12 +9,45 @@ class App extends Component{
     this.state = {currentUser:null};
   }
 
+  fetchNewUserData = () =>{
+    return fetch('https://randomuser.me/api/', 
+    {method : "GET",
+      headers: {
+        'Accept' : 'application/json',
+        'Content-Type': 'application/json'
+      }
+    }).then(response => response.json())
+    .then(data => {return data})
+    .catch(err => console.log(err.message))
+  }
+
+  componentDidMount(){
+    this.fetchNewUserData().then( (data) =>{
+      this.setState({currentUser:data})
+    })
+  }
+
+
+
   render(){
+    const {currentUser} = this.state;
+    
+    if (!currentUser) {
+      return(
+        <div className="App">
+        <p>
+          loading data...
+        </p>
+      </div>
+      )
+    }
+    console.log(currentUser.picture)
     return(
       <div className="App">
         <p>
-          main component
+          {JSON.stringify(currentUser)}
         </p>
+        <img src={currentUser.picture} alt="image"/>
       </div>
     )
   }
