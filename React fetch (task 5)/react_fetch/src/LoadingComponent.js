922a33846a58630f1170773b8dcb32a183a9a058
polon59 <przemyslaw.polczak@gmail.com>
2019-05-31 02:12:15 +0200

    Add rendered content in 'loadingComponent.js'

--- React fetch (task 5)/react_fetch/src/LoadingComponent.js
+++ React fetch (task 5)/react_fetch/src/LoadingComponent.js
@@ -1,7 +1,11 @@
 import React from 'react'
 
 const LoadingComponent = (props) =>{
-    return({
-        
-    })
+    return(
+        <div className="App">
+        <p>
+            loading data...
+        </p>
+        </div>
+    )
 }
