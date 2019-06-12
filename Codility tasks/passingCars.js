c25ce37043d4a5b682e0bb6430693e6a5aaf89b6
polon59 <przemyslaw.polczak@gmail.com>
2019-06-12 16:38:05 +0200

    Add passingCars.js' file

---
+++ Codility tasks/passingCars.js
@@ -0,0 +1,29 @@
+// you can write to stdout for debugging purposes, e.g.
+// console.log('this is a debug message');
+
+function solution(A) {
+    // write your code in JavaScript (Node.js 8.9.4)
+    let pairs = 0;
+    let eastCount = 0;
+
+    for (const element of A) {
+        element === 0 ? eastCount++ : pairs += eastCount;
+        if (pairs > 1000000000) {
+            return -1;
+        }
+    }
+
+
+    // A.forEach(element => {
+    //     element === 0 ? eastCount++ : pairs += eastCount;
+    //     if (pairs > 1000000000) {
+    //         console.log('dupa')
+    //         return -1;
+    //     }
+    // });
+
+    return pairs;
+}
+
+
+console.log(solution([0,1,0,1,1]))
